7016b8d0bd7994204a4881a754c82dd6b4a58b0a
Alexandre Souza dos Santos <alexandre.souza.office@gmail.com>
2026-05-13 11:48:17 -0300

    retificação do edital

--- script.js
+++ script.js
@@ -9,15 +9,40 @@ const retificacoes = [
     antes: "02/04 a 10/05/2026",
     depois: "06/04 a 10/05/2026",
   },
+  {
+    titulo: "Retificação 03 (V2) - Período de inscrição",
+    antes: "22/03 a 05/04/2026",
+    depois: "Até 17/05/2026",
+  },
+  {
+    titulo: "Retificação 04 (V2) - Período de submissão",
+    antes: "06/04 a 10/05/2026",
+    depois: "06/04 a 17/05/2026",
+  },
+  {
+    titulo: "Retificação 05 (V2) - Triagem e validação",
+    antes: "11/05 a 15/05/2026",
+    depois: "18/05 a 22/05/2026",
+  },
+  {
+    titulo: "Retificação 06 (V2) - Publicação dos vídeos e início da votação",
+    antes: "16/05/2026",
+    depois: "23/05/2026",
+  },
+  {
+    titulo: "Retificação 07 (V2) - Votação popular",
+    antes: "16/05 a 31/05/2026",
+    depois: "23/05 a 31/05/2026",
+  },
 ];
 
 const cronograma = [
   { etapa: "Publicação do edital", data: "22/03/2026" },
-  { etapa: "Inscrições", data: "22/03 a 05/04/2026" },
-  { etapa: "Submissão das composições", data: "06/04 a 10/05/2026" },
-  { etapa: "Triagem e validação", data: "11/05 a 15/05/2026" },
-  { etapa: "Publicação dos vídeos e início da votação", data: "16/05/2026" },
-  { etapa: "Votação popular", data: "16/05 a 31/05/2026" },
+  { etapa: "Inscrições", data: "Até 17/05/2026" },
+  { etapa: "Submissão das composições", data: "06/04 a 17/05/2026" },
+  { etapa: "Triagem e validação", data: "18/05 a 22/05/2026" },
+  { etapa: "Publicação dos vídeos e início da votação", data: "23/05/2026" },
+  { etapa: "Votação popular", data: "23/05 a 31/05/2026" },
   { etapa: "Avaliação do júri técnico", data: "29/05 a 12/06/2026" },
   { etapa: "Fechamento das avaliações", data: "12/06/2026" },
   { etapa: "Divulgação do vencedor", data: "13 ou 14/06/2026" },
